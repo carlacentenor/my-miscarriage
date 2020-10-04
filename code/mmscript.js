c016be4c7c72090c7962310fa8667681e946c396
idsintehittapa <jblom15@gmail.com>
2020-10-04 14:55:10 +0200

    Uploaded mm project, started cleaning code for INDEX page, started MQ on phone

---
+++ code/mmscript.js
@@ -0,0 +1,19 @@
+// so this is how to make a coment!
+
+/* between these 
+are 
+commenting out like # in python
+*/
+var a = "Hello ", b = "world", c = " wtf";
+
+// this makes it happen by default in consol
+console.log(a+b+c);
+
+// DOCUMENT - what makes it visibile for the user
+// document.write();
+
+
+// objects gives the possibility to store multiple values within one varible. It's like a dictonary {}
+
+// Maybe the JS can be used to update the page with info when published? and the graphs? Mayve not??
+
